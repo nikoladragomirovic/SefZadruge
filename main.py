b295001d83ae3ab8b65321c491f50b4205a02763
z1cLa <miljanlazic@gmail.com>
2023-09-29 16:08:24 +0200

    bugFix

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ TOKEN: Final = '6437235439:AAF39_by7pIwn1-_oXwQnj8qH9hoE90cZec'
 BOT_USERNAME: Final = '@ZadrugaS69_bot'
 
 # Commands
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE)
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('UZMI GA ZDVIJE')
