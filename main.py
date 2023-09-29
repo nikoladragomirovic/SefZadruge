2862b85a0e84487af948d2bd2a8b57cf6a0002ff
z1cLa <miljanlazic@gmail.com>
2023-09-29 16:06:29 +0200

    Added telegram extensions and start command

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('UZMI GA ZDVIJE')
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '6437235439:AAF39_by7pIwn1-_oXwQnj8qH9hoE90cZec'
+BOT_USERNAME: Final = '@ZadrugaS69_bot'
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE)
+    await update.message.reply_text('UZMI GA ZDVIJE')
